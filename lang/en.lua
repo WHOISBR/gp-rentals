8f9dd0748b634868c75c7eb513932e06c4a67786
P Gergő <69343477+Rav3n95@users.noreply.github.com>
2022-02-24 16:49:33 +0100

    Update en.lua

--- lang/en.lua
+++ lang/en.lua
@@ -1,17 +1,25 @@
 local Translations = {
     info = {
-        ["air_veh"] = "Aircraft Rentals",
-        ["land_veh"] = "Vehicle Rentals",
-        ["sea_veh"] = "Boat Rentals",
+        ["header"] = "Vehicle Rentals",
+        ['veh_returned'] = "Vehicle Returned!",
+        ['paid_deposit'] = "Paid $%{value} deposit", 
+        ['refound'] = " $ refounded in cash"
     },
     error = {
-        ["not_enough_space"] = "%{vehicle} is in the way!",
+        ["not_enough_space"] = "Something blocking the way!",
         ["not_enough_money"] = "You do not have enough money!",
         ["no_vehicle"] = "No vehicle to return!"
     },
     task = {
-        ["return_veh"] = "Return your rented vehicle.",
-        ['veh_returned'] = 'Vehicle Returned!'
+        ["return_veh"] = "Return vehicle",
+        ["rent_land"] = "Rent a car",
+        ["rent_air"] = "Rent a plane",
+        ["rent_water"] = "Rent a boat",
+    },
+    blip = {
+        ["land"] = 'Vehicle rental',
+        ["air"] = 'Airplane rental',
+        ["water"] = 'Boat rental'
     }
 }
 Lang = Locale:new({phrases = Translations, warnOnMissing = true})
